063b4106710d38b341cca9153e53b84baa11c657
sonam gupta <2997sonamgeda@gmail.com>
2025-09-29 11:54:06 +0530

    auth-service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = "http://localhost:3000/api/auth";
+
+  constructor(private http: HttpClient) { }
+
+  // Register
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+
+  // login
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+
+  //Save Token
+  saveToken(token: string): void {
+    localStorage.setItem('authToken', token);
+  }
+
+  // Get Token
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  // Logout
+  logout(): void {
+    localStorage.removeItem('authToken');
+  }
+
+  // Check if user is Logged In
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
+
 }
